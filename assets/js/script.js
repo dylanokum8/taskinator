8575b9d7f0253c502c5576d5d6b1688dd44571ae
Dylan Okum <dylanokum@icloud.com>
2022-04-19 21:51:18 -0400

    added additional functions to the task form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,37 @@
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
-// console.log(event);
-  event.preventDefault();
+var createTaskHandler = function(event) {
+ 
 
+  event.preventDefault();
+  var taskNameInput = document.querySelector("input[name='task-name']").value;
+  var taskTypeInput = document.querySelector("select[name='task-type']").value;
+  
+  /* CREATE LIST ITEM */
   var listItemEl = document.createElement("li");
   listItemEl.className = "task-item";
-  listItemEl.textContent = "This is a new task.";
-  tasksToDoEl.appendChild(listItemEl);
+
+  // CREATE DIV TO HOLD TASK INFO AND ADD TO LIST ITEM //
+  var taskInfoEl = document.createElement("div");
+  // GIVE IT A CLASS NAME
+  taskInfoEl.className = "task-info";
+  // add HTML Content to DIV // 
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+
+  listItemEl.appendChild(taskInfoEl);
+
+  // add entire list item to list
+tasksToDoEl.appendChild(listItemEl);
+
+
 };
 
 formEl.addEventListener("submit", createTaskHandler);
+
+
+
+
+
+
+
